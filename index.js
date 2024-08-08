0eed8441eac2b545e13bbe902fbca37b82db0cf2
Bot WhatsApp HACKING <154031706+HACKING995@users.noreply.github.com>
2024-08-08 20:06:47 +0000

    Update index.js

--- index.js
+++ index.js
@@ -249,38 +249,18 @@
                 };
 // Chat_bot 
 
-let lastResponded = 0; // Variable pour garder track du dernier message répondu
-let lastSender = null; // Variable pour garder track du dernier expéditeur
-let isResponding = false; // Variable pour indiquer si le bot est en train de répondre
 
 if (conf.CHAT_BOT === 'oui') {
-  // Vérifier si le message provient d'une autre personne
-  if (_0x45f822.endsWith("@s.whatsapp.net") && _0x45f822 !== lastSender) {
-    // Vérifier si le message est un texte
-    if (_0x41edd6) {
-      // Vérifier que le bot n'est pas en train de répondre
-      if (!isResponding) {
-        isResponding = true; // Indiquer que le bot est en train de répondre
-
-        fetch(`http://api.brainshop.ai/get?bid=177607&key=NwzhALqeO1kubFVD&uid=[uid]&msg=${_0x41edd6}`)
-          .then(response => response.json())
-          .then(data => {
-            const botResponse = data.cnt;
-            console.log(botResponse);
-            _0x540c51(botResponse);
-            lastResponded = Date.now(); // Mettre à jour la dernière fois que le bot a répondu
-            lastSender = _0x45f822; // Mettre à jour le dernier expéditeur
-            isResponding = false; // Indiquer que le bot a fini de répondre
-          })
-          .catch(error => {
-            console.error("Erreur lors de la requête à BrainShop :", error);
-            isResponding = false; // Indiquer que le bot a fini de répondre
-          });
-      }
-    }
-  }
+  fetch("https://api.brainshop.ai/get?bid=182939&key=Je69ped2ZzbfNf3g&uid=[uid]&msg=[msg][uid]&msg=" + texte)
+    .then(response => response.json()) 
+    .then(data => {
+      const respmsg = data.cnt;
+      repondre(respmsg);
+    })
+    .catch(error => {
+      console.error("Erreur lors de la requête à BrainShop :", error);
+    });
 }
-
             
                     //fin Chat_bot
                 
