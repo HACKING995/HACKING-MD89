9f124212119ecbc5feec4757c07687d6f8ce5e08
Bot WhatsApp HACKING <154031706+HACKING995@users.noreply.github.com>
2024-08-08 20:38:56 +0000

    Update index.js

--- index.js
+++ index.js
@@ -249,18 +249,57 @@
                 };
 // Chat_bot 
 
+let lastResponded = 0; // Variable pour garder track du dernier message répondu
+
+let lastSender = null; // Variable pour garder track du dernier expéditeur
+
+let isResponding = false; // Variable pour indiquer si le bot est en train de répondre
 
 if (conf.CHAT_BOT === 'oui') {
-  fetch("https://api.brainshop.ai/get?bid=182939&key=Je69ped2ZzbfNf3g&uid=[uid]&msg=[msg][uid]&msg=" + texte)
-    .then(response => response.json()) 
-    .then(data => {
-      const respmsg = data.cnt;
-      repondre(respmsg);
-    })
-    .catch(error => {
-      console.error("Erreur lors de la requête à BrainShop :", error);
-    });
+
+// Vérifier si le message provient d'une autre personne
+
+if (auteurMessage.endsWith("s.whatsapp.net") && auteurMessage !== lastSender) {
+
+// Vérifier si le message est un texte
+
+if (texte) {
+
+// Vérifier que le bot n'est pas en train de répondre
+
+if (!isResponding) {
+
+isResponding = true; // Indiquer que le bot est en train de répondre
+
+fetch(`http://api.brainshop.ai/get?bid=177607&key=NwzhALqeO1kubFVD&uid=[uid]&msg=${message}`)
+
+.then(response => response.json())
+
+.then(data => {
+
+const botResponse = data.cnt;
+
+console.log(message);
+
+repondre(botResponse);
+
+lastResponded = Date.now(); // Mettre à jour la dernière fois que le bot a répondu
+
+lastSender = auteurMessage; // Mettre à jour le dernier expéditeur
+
+isResponding = false; // Indiquer que le bot a fini de répondre
+
+});
+
 }
+
+}
+
+}
+
+}
+
+
             
                     //fin Chat_bot
                 
